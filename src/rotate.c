08732a1bff5b67eff8e50b8247c65fca53110375
palmaoff <fedorin.anton@mail.ru>
2020-03-09 18:36:24 +0300

    hellhell

--- src/rotate.c
+++ src/rotate.c
@@ -19,14 +19,14 @@ void	rotate(t_camera cam, double *x, double *y, double *z)
 	double pre_z;
 
 	pre_y = *y;
-	*y = pre_y * cos(cam.dir.x) + *z * sin(cam.dir.x);
-	*z = -pre_y * sin(cam.dir.x) + *z * cos(cam.dir.x);
+	*y = pre_y * cos(cam.x_r) + *z * sin(cam.x_r);
+	*z = -pre_y * sin(cam.x_r) + *z * cos(cam.x_r);
 
 	pre_z = *z;
-	*z = *x * sin(cam.dir.y) + pre_z * cos(cam.dir.y);
-	*x = pre_z * sin(cam.dir.y) + *x * cos(cam.dir.y);
+	*z = *x * sin(cam.y_r) + pre_z * cos(cam.y_r);
+	*x = pre_z * sin(cam.y_r) + *x * cos(cam.y_r);
 
 	pre_x = *x;
-	*x = pre_x * cos(cam.dir.z) - *y * sin(cam.dir.z);
-	*y = pre_x * sin(cam.dir.z) + *y * cos(cam.dir.z);
+	*x = pre_x * cos(0) - *y * sin(0);
+	*y = pre_x * sin(0) + *y * cos(0);
 }
