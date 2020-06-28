953a06f10f344f5b0cfe38f30e8cde260a429c4e
trover97 <53858027+trover97@users.noreply.github.com>
2020-06-28 13:26:48 +0300

    fix Ambient and fix output text

--- src/help.c
+++ src/help.c
@@ -6,7 +6,7 @@
 /*   By: wquirrel <wquirrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 16:36:36 by wquirrel          #+#    #+#             */
-/*   Updated: 2020/06/26 00:32:46 by wquirrel         ###   ########.fr       */
+/*   Updated: 2020/06/28 13:18:11 by wquirrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,13 @@ void output_help()
 	ft_putendl("Syntax requirements for scene:\n");
 	ft_putendl("## NO TABULATION between words(separated by spaces)"
 		   " ONLY SPACES");
-	ft_putendl("## Add tags for \"scene\" and \"objects\"");
+	ft_putendl("## Add tags \"scene\" (camera & lights) and \"objects\" "
+			"(figures)");
 	ft_putendl("## Add CURLY brackets for all text objects");
-	ft_putendl("## CURLY brackets \"scene\" and \"camera\": \"{\" & \"};\"");
+	ft_putendl("## CURLY brackets \"scene\" and \"objects\": \"{\" & \"};\"");
 	ft_putendl("## CURLY brackets other: \"{\" & \"}\"");
 	ft_putendl("## Add USUAL brackets for all configurations + spaces");
 	ft_putendl("## New line \"\\n\" after curly brackets, tags and config");
 	ft_putendl("## For example, use prepared scenes");
+	ft_putendl("## Full information in README");
 }
